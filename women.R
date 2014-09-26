0a5de4fbc9f0759ca81f8ef01f0ba2c051f28add
cjfschneider <corschneider@yahoo.de>
2014-09-26 11:48:22 +0200

    update

--- women.R
+++ women.R
@@ -5,11 +5,12 @@
 # Pick the dataset
 data(women)
 ?women
-# This data set gives the average heights (in) and 
+# This data set provides the average heights (in) and 
 # weights (lbs) for American women aged 30–39.
 
 # What are the variables?
 names(women)
+# height & weight 
 
 # Convert the variables in centimeters and kilograms
 women$height_cm<-women$height*2.54
@@ -18,15 +19,19 @@ women$weight_kg<-women$weight*0.45359237
 # Descriptive Statistics on the Distribution
 #########
 summary(women)
+# Women's height in this dataset is between 147.3 and 182.9 cm
+# and the median equals the mean with 165.1 cm.
+# Women's weight in this dataset is between 52.15 and 74.39 km
+# with a median of 61.23 kg and a mean of 62.02 kg.
 
 # Histogram for Height
 hist(women$height_cm,
      main='Average Height for American Women',
      xlab='Height in Centimeters') 
-# Women's height in this dataset is between 147.3 and 182.9 cm
-# and the median equals the mean with 165.1 cm.
-# Women's weight in this dataset is between 52.15 and 74.39 km
-# with a median of 61.23 kg and a mean of 62.02 kg.
+# Most women have a height between 160 and 180 cm (8 women). 
+# Only two are between 140 and 150 centimeters and two are 
+# between 180 and 190 centimeters. 
+# In a first impression this is a slighly right skewed normal distribution.
 
 # Histogram for Weight
 hist(women$weight_kg,
